3b7c337a0228050e89137830b30de1e3ad21d758
behnambm <behnam.mohammadzadeh21@gmail.com>
2019-02-12 21:19:16 +0330

    the version before of this commit is stable :)

--- fetch_chat_history.php
+++ fetch_chat_history.php
@@ -12,6 +12,6 @@ $stmt = $con->prepare("UPDATE chat_message SET is_seen = ? WHERE from_user_id = 
 $stmt->execute(array('1' , $_POST['to_user_id'] , $_SESSION['user_id']));
 echo fetch_chat_history($_SESSION['user_id'] , $_POST['to_user_id'] );
 
-
+ 
 
 
