9e95d866c62a08ee7192c41e1abdd4a91d7e5bf5
behnambm <behnam.mohamadzadeh21@gmail.com>
2019-03-12 13:08:13 +0330

    fixing a bug in DB inserting (id_per_msg)

--- insert_chat.php
+++ insert_chat.php
@@ -3,15 +3,13 @@ session_start();
 require_once 'functions.php';  
 date_default_timezone_set('Asia/Tehran'); 
 ini_set('display_errors',1);
-if (isset($_POST['to_user_id'], $_POST['chat_message'])) {
-    $stmt = $con->prepare("SELECT id_per_msg FROM chat_message WHERE from_user_id = ? AND to_user_id = ? ORDER BY id_per_msg DESC LIMIT 1");
-    $stmt->execute(array($_SESSION['user_id'], $_POST['to_user_id']));
+if (isset($_SESSION['logged_in'], $_POST['to_user_id'], $_POST['chat_message'])) {
+    $stmt = $con->prepare("SELECT id_per_msg FROM chat_message WHERE (from_user_id = ? AND to_user_id = ?) OR (from_user_id = ? AND to_user_id = ?) ORDER BY id_per_msg DESC LIMIT 1");
+    $stmt->execute(array($_SESSION['user_id'], $_POST['to_user_id'], $_POST['to_user_id'], $_SESSION['user_id']));
     $res = $stmt->fetchAll();
     $msg_id = null;
-    foreach($res as $row){
-
+    foreach($res as $row)
         $msg_id = (int)$row['id_per_msg']+1;
-    }
     $current_time = date('Y-m-d H:i:s'); 
     $stmt = $con->prepare("INSERT INTO chat_message(to_user_id,from_user_id,chat_message, timestamp ,is_seen ,is_sent,id_per_msg) VALUES(?,?,?,?,?,?,?)"); 
     if ($stmt->execute(array($_POST['to_user_id'], $_SESSION['user_id'], $_POST['chat_message'], $current_time, '0', '1',$msg_id))) {
