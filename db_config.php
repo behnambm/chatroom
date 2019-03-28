e4722fea494170e93d9da7ee25b092d6aa2a2ef8
behnambm <behnam.mohamadzadeh21@gmail.com>
2019-03-28 21:01:18 +0430

    insert default user (Owner)

--- db_config.php
+++ db_config.php
@@ -7,7 +7,7 @@ $db_name = 'behnam_db';
 
 function setup(){
     $db_file_content = file_get_contents('db.conf');
-    if($db_file_content == "DATABASE_CONNECT::1;CREATE_DB::1;USE_DATABASE::1;USERS_TBL::1;COOKIE_TBL::1;LOGIN_DETAIL_TBL::1;CHAT_MSG_TBL::1;ADMIN_TBL::1;"){
+    if($db_file_content == "DATABASE_CONNECT::1;CREATE_DB::1;USE_DATABASE::1;USERS_TBL::1;COOKIE_TBL::1;LOGIN_DETAIL_TBL::1;CHAT_MSG_TBL::1;ADMIN_TBL::1;INSERT_USER::1;INSERT_HASH::1;INSERT_PRIVILAGE::1;"){
         return true;
     }
     global $db_host,$db_name,$db_pass,$db_user;
@@ -79,8 +79,23 @@ function setup(){
         )";
         $con->exec($sql6);
         file_put_contents('db.conf','ADMIN_TBL::1;', FILE_APPEND);
+
         // create root user 
+        $sql7 = "INSERT INTO users (id, username, password, display_name, email, profile_pic) VALUES (1, 'admin', '5d1ea37d3a602f792b251568fa9ba967', 'مدیر اصلی', 'admin@email.com', 'files/images/crown.png')";
+        $con->exec($sql7);
+        file_put_contents('db.conf', 'INSERT_USER::1;', FILE_APPEND);
+
+        // insert cookie hash in table 
+        $sql8 = "INSERT INTO cookie_id (id, cookie_hash) VALUES (1, '42317fe0b4e71cd07bc4c406f88f693141690a93')";
+        $con->exec($sql8);
+        file_put_contents('db.conf', 'INSERT_HASH::1;', FILE_APPEND);
+
+        // insert owner privilage in table
+        $sql9 = "INSERT INTO admin_tbl (username, privilage) VALUES ('admin','owner')";
+        $con->exec($sql9);
+        file_put_contents('db.conf', 'INSERT_PRIVILAGE::1;', FILE_APPEND);
+
     }catch(PDOException $e){
-    echo $sql . "<br>" . $e->getMessage();
+        echo $sql . "<br>" . $e->getMessage();
     }
 }
