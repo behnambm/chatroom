db6de039df585cffa1f60d720322a7f9eeb65b76
behnambm <behnam.mohamadzadeh21@gmail.com>
2019-03-28 20:30:38 +0430

    fixing a bug in db configuration

--- db_config.php
+++ db_config.php
@@ -7,24 +7,24 @@ $db_name = 'behnam_db';
 
 function setup(){
     $db_file_content = file_get_contents('db.conf');
-    if($db_file_content == 'DATABASE_CONNECT::1;IS_DATABASE_CREATED::1;USE_DATABASE::1;IS_USERS_TABLE_CREATED::1;IS_COOKIE_TABLE_CREATED::1;IS_LOGIN_DETAILS_TABLE_CREATED::1;IS_CHAT_MESSAGE_TABLE_CREATED::1;IS_ADMIN_TABLE_CREATED::1;'){
+    if($db_file_content == "DATABASE_CONNECT::1;CREATE_DB::1;USE_DATABASE::1;USERS_TBL::1;COOKIE_TBL::1;LOGIN_DETAIL_TBL::1;CHAT_MSG_TBL::1;ADMIN_TBL::1;"){
         return true;
     }
     global $db_host,$db_name,$db_pass,$db_user;
     try{
         $con = new PDO('mysql:host='.$db_host.';charset=utf8;',$db_user,$db_pass);
-        file_put_contents('db.conf','DATABASE_CONNECT::1;'.PHP_EOL);
+        file_put_contents('db.conf','DATABASE_CONNECT::1;');
         // set the PDO error mode to exception
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         // create a database
         $sql = "CREATE DATABASE IF NOT EXISTS behnam_db CHARACTER SET utf8 COLLATE utf8_persian_ci;";
         $con->exec($sql);
-        file_put_contents('db.conf','IS_DATABASE_CREATED::1;'.PHP_EOL,FILE_APPEND);
+        file_put_contents('db.conf','CREATE_DB::1;',FILE_APPEND);
 
         //select database
         $con->exec('USE behnam_db;');
-        file_put_contents('db.conf','USE_DATABASE::1;'.PHP_EOL,FILE_APPEND);
+        file_put_contents('db.conf','USE_DATABASE::1;',FILE_APPEND);
 
         // create users table
         $sql2 = "CREATE TABLE IF NOT EXISTS users (
@@ -36,7 +36,7 @@ function setup(){
     		profile_pic varchar(64) NOT NULL
             );";
         $con->exec($sql2);
-        file_put_contents('db.conf','IS_USERS_TABLE_CREATED::1;'.PHP_EOL,FILE_APPEND);
+        file_put_contents('db.conf','USERS_TBL::1;',FILE_APPEND);
 
         // create cookies table
         $sql3 = "CREATE TABLE IF NOT EXISTS cookie_id (
@@ -44,7 +44,7 @@ function setup(){
             cookie_hash varchar(128) UNIQUE NOT NULL
             );";
         $con->exec($sql3);
-        file_put_contents('db.conf','IS_COOKIE_TABLE_CREATED::1;'.PHP_EOL,FILE_APPEND);
+        file_put_contents('db.conf','COOKIE_TBL::1;',FILE_APPEND);
 
         // create login_details TABLE
         $sql4 = "CREATE TABLE IF NOT EXISTS `login_details` (
@@ -55,7 +55,7 @@ function setup(){
             typing_target VARCHAR(2)
           );";
         $con->exec($sql4);
-        file_put_contents('db.conf','IS_LOGIN_DETAILS_TABLE_CREATED::1;'.PHP_EOL, FILE_APPEND);
+        file_put_contents('db.conf','LOGIN_DETAIL_TBL::1;', FILE_APPEND);
 
         // create chat_message TABLE
         $sql5 = "CREATE TABLE IF NOT EXISTS chat_message (
@@ -69,7 +69,7 @@ function setup(){
                 id_per_msg INT DEFAULT 0
             );";
         $con->exec($sql5);
-        file_put_contents('db.conf','IS_CHAT_MESSAGE_TABLE_CREATED::1;'.PHP_EOL, FILE_APPEND);
+        file_put_contents('db.conf','CHAT_MSG_TBL::1;', FILE_APPEND);
 
         // create admin TABLE 
         $sql6 = "CREATE TABLE IF NOt EXISTS admin_tbl (
@@ -78,7 +78,7 @@ function setup(){
             privilage VARCHAR(8) NOT NULL 
         )";
         $con->exec($sql6);
-        file_put_contents('db.conf','IS_ADMIN_TABLE_CREATED::1;'.PHP_EOL, FILE_APPEND);
+        file_put_contents('db.conf','ADMIN_TBL::1;', FILE_APPEND);
         // create root user 
     }catch(PDOException $e){
     echo $sql . "<br>" . $e->getMessage();
