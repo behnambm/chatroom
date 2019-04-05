63deab82bfb59cf1f6855fca57b904dea5dee2f3
behnambm <behnam.mohamadzadeh21@gmail.com>
2019-04-05 10:56:37 +0430

    fixing register page responsivity (no bootstrap only css)

--- register.php
+++ register.php
@@ -19,86 +19,90 @@ if(isset($_COOKIE['logged_in'],$_COOKIE['hash']) &&  $_COOKIE['logged_in'] = 'ye
   <link rel="stylesheet" href="files/css/bootstrap-4.1.2.min.css">
     <link rel="stylesheet" href="files/dist/cropper.css">
     <link rel="stylesheet" href="files/css/style.css">
+
 </head>
 <body>
-     <div class="container">   
-        <div class="wrapper reg-wrapper">
-            <form action="" method="post" enctype="multipart/form-data" id="register-form" autocomplete="off" >
-                <span id="register-img"><i class="fa fa-user-plus"></i><h1>ثبت نام</h1></span>
-                <div class="clear"></div>
-                <div class="input-div" id="user-div">
-                    <span id="icon-holder-user">
-                        <i class="fa fa-user"></i>
-                    </span>
-                    <input type="text" name="regusername" placeholder="نام کاربری " id="username-input" required >
-                </div><div class="clear"></div><div class="err err-user">این نام کاربری قبلا ثبت شده است.</div>
-                <div class="input-div" id="displayname-div">
-                    <span id="icon-holder-displayname">
-                        <i class="fa fa-eye"></i>
-                    </span>
-                    <input type="text" name="regdisplayname" placeholder="نام (جهت نمایش)" id="displayname-input"  required >
-                </div><div class="clear"></div>
-                <div class="input-div" id="email-div">
-                    <span id="icon-holder-email">
-                        <i class="fa fa-envelope"></i>
-                    </span>
-                    <input type="email" name="regemail" placeholder="ایمیل" id="email-input" required >
-                </div><div class="clear"></div><div class="err err-email">این ایمیل قبلا ثبت شده است.</div>
-                <div class="input-div" id="pass-div">
-                    <span id="icon-holder-pass">
-                        <i class="fa fa-lock"></i>
-                    </span>
-                    <input type="password" name="regpassword" placeholder="رمز عبور " id="password-input" required >
-                </div><div class="clear"></div>
-                <div class="input-div" id="profilepic-div">
-                    <label class="label" data-toggle="tooltip">
-                        <img class="rounded" id="avatar" src="files/images/user.png" alt="avatar">
-                        <input type="file" class="sr-only" id="input" name="image" accept="image/*">
-                    </label>
-                    <label for="input" id="icon-holder-profilepic">
-                            انتخاب عکس پروفایل
-                    </label>
-                 </div><div class="clear"></div>
-                <button type="submit" class="reg-btn" id="sub-btn">ثبت نام</button>
-                <div class="loading">
-                    <div class="obj"></div>
-                    <div class="obj"></div>
-                    <div class="obj"></div>
-                    <div class="obj"></div>
-                    <div class="obj"></div>
-                    <div class="obj"></div>
-                    <div class="obj"></div>
-                    <div class="obj"></div>
-                </div>
-            </form>
-            <div class="clear"></div>
-            <div class="line">
-                <div class="inner-line"></div>
-            </div>
-            <a href="login.php" id="login">ورود به حساب کاربری</a>
-        </div>
-    <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="modalLabel">برش عکس</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="img-container">
-                        <img id="image" src="files/images/user.png">
+     <div class="container register-page">  
+        <div class="row"> 
+            <div class="wrapper reg-wrapper col-xs-push-1 col-xs-10 col-sm-push-3 col-sm-6 col-md-push-4 col-md-4 col-lg-3">
+                <form action="" method="post" enctype="multipart/form-data" id="register-form" autocomplete="off" >
+                    <span id="register-img"><i class="fa fa-user-plus"></i><h1>ثبت نام</h1></span>
+                    <div class="clear"></div>
+                    <div class="input-div" id="user-div">
+                        <span id="icon-holder-user">
+                            <i class="fa fa-user"></i>
+                        </span>
+                        <input type="text" name="regusername" placeholder="نام کاربری " id="username-input" required >
+                    </div><div class="clear"></div><div class="err err-user">این نام کاربری قبلا ثبت شده است.</div>
+                    <div class="input-div" id="displayname-div">
+                        <span id="icon-holder-displayname">
+                            <i class="fa fa-eye"></i>
+                        </span>
+                        <input type="text" name="regdisplayname" placeholder="نام (جهت نمایش)" id="displayname-input"  required >
+                    </div><div class="clear"></div>
+                    <div class="input-div" id="email-div">
+                        <span id="icon-holder-email">
+                            <i class="fa fa-envelope"></i>
+                        </span>
+                        <input type="email" name="regemail" placeholder="ایمیل" id="email-input" required >
+                    </div><div class="clear"></div><div class="err err-email">این ایمیل قبلا ثبت شده است.</div>
+                    <div class="input-div" id="pass-div">
+                        <span id="icon-holder-pass">
+                            <i class="fa fa-lock"></i>
+                        </span>
+                        <input type="password" name="regpassword" placeholder="رمز عبور " id="password-input" required >
+                    </div><div class="clear"></div>
+                    <div class="input-div" id="profilepic-div">
+                        <label class="label" data-toggle="tooltip">
+                            <img class="rounded" id="avatar" src="files/images/user.png" alt="avatar">
+                            <input type="file" class="sr-only" id="input" name="image" accept="image/*">
+                        </label>
+                        <label for="input" id="icon-holder-profilepic">
+                                انتخاب عکس پروفایل
+                        </label>
+                    </div><div class="clear"></div>
+                    <button type="submit" class="reg-btn" id="sub-btn">ثبت نام</button>
+                    <div class="loading">
+                        <div class="obj"></div>
+                        <div class="obj"></div>
+                        <div class="obj"></div>
+                        <div class="obj"></div>
+                        <div class="obj"></div>
+                        <div class="obj"></div>
+                        <div class="obj"></div>
+                        <div class="obj"></div>
                     </div>
+                </form>
+                <div class="clear"></div>
+                <div class="line">
+                    <div class="inner-line"></div>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">لغو</button>
-                    <button type="button" class="btn btn-primary" id="crop">برش</button>
+                <a href="login.php" id="login">ورود به حساب کاربری</a>
+            </div>
+        </div>
+ 
+
+<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true"><!-- picture select section -->
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalLabel">برش عکس</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="img-container">
+                    <img id="image" src="files/images/user.png">
                 </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">لغو</button>
+                <button type="button" class="btn btn-primary" id="crop">برش</button>
             </div>
         </div>
     </div>
-</div>  
+</div>  <!-- picture select section -->
 <script src="files/js/jquery-3.1.1.js"></script>
 <script src="files/js/bootstrap.bundle.min.js"></script>
 <script src="files/dist/cropper.js"></script>
@@ -167,12 +171,6 @@ window.addEventListener('DOMContentLoaded', function () {
 //******************************************************************************************************
 $(document).ready(()=>{
     console.log(formDATA.get('avatar'));
-    let fixContainer = ()=>{
-        let ww = $(window).width();
-        $('.container').width(ww);
-    }
-    fixContainer();
-    $(window).resize(fixContainer);
     $('#register-form input').focus((e)=>{   // this code makes inputs animated
         let id = $(e.target).attr('id');
         if(id == 'username-input'){
